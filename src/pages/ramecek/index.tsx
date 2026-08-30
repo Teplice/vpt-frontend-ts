a057ec993e3dbf2660d776b122c2eea3fe6d96c5
Teplice <teplice.board@gmail.com>
2026-08-30 15:56:32 +0200

    FB frame page update

--- src/pages/ramecek/index.tsx
+++ src/pages/ramecek/index.tsx
@@ -7,7 +7,9 @@ const FramePage: NextPage = () => {
         <ul className="aktuality__list">
             <li className="aktuality__list-item">
                 <div className="aktualita-card__text-container">
-                    <h2 className="heading-secondary aktualita-card__heading">Editor profilové fotky s rámečkem</h2>
+                    <h2 className="heading-secondary aktualita-card__heading">Editor profilové fotky pro sociální sítě s rámečkem</h2>
+                    <div className="aktualita-card__message">Vytvořte si profilovou fotku pro Facebook nebo Instagram s rámečkem Volby pro Teplice a podpořte nás!
+                    </div>
                     <div className="aktualita-card__message">
                         Nahrajte svojí fotku, upravte velikost a umístění a stáhněte si hotovou fotku s naším rámečkem
                     </div>
